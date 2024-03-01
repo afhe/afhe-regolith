f6365fe6f5de023e9346f4dd3436ee666e29b6ec
afhe <afhe99@gmail.com>
2024-03-01 15:23:14 +1100

    remove

--- scripts/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-screenshot_file=/tmp/screenshot.png
-
-function notify_failed {
-    notify-send -a gnome-screenshot -u low "Screenshot failed."
-}
-# Take screenshot
-/usr/bin/gnome-screenshot -a -f "$screenshot_file" && /usr/bin/xclip -selection clipboard -t image/png -i "$screenshot_file" || notify_failed
