ad7974d591956f051642d10560490c89e77b9115
afhe <afhe99@gmail.com>
2024-03-18 16:11:55 +1100

    clean up screenshot script again..

--- regolith/scripts/screenshot.sh
+++ regolith/scripts/screenshot.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-screenshot_file=/tmp/screenshot.png
-
-function notify_failed() {
-    notify-send -a "$1" -u low "$2"
+function notify() {
+    notify-send -a "$1" -u normal "$2"
 }
-# Take screenshot
-gnome_errors=$(gnome-screenshot -acf "$screenshot_file") 
-if [[ ${#gnome_errors} -gt 0 ]]; then
-    notify_failed "gnome-screenshot" "$gnome_errors"
-fi
 
-if [[ -e "$screenshot_file" ]]; then
-    cat "$screenshot_file" |  xclip -i -selection clipboard -target image/png || notify_failed "xclip" "Failed"
-    rm "$screenshot_file"
-fi 
+gnome-screenshot -acf /tmp/test && cat /tmp/test | 
+xclip -i -selection clipboard -target image/png && 
+notify "screenshot" "Captured" || 
+notify "screenshot" "Failed"
